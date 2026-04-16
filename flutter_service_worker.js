f89dd616725d7a12202952457aae59cbd9e58924
bilal2000088-hash <bilal.2000088@gmail.com>
2026-04-16 23:27:22 +0300

    Deploy Flutter web to GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "2f6ef905aaac60a3b4d0c54a3c33e483
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "0349008c92f2954b257ba470c6fa25c9",
+"flutter_bootstrap.js": "ff9a88ba192d397121789670d06d80ce",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
