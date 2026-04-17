1b79217bb6a0309e35834f96742c026e624da2c9
bilal2000088-hash <bilal.2000088@gmail.com>
2026-04-17 13:34:24 +0300

    Redeploy web app with updated settings

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "2f6ef905aaac60a3b4d0c54a3c33e483
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "4ad371617b53dc7d7b04932a34ffd396",
+"flutter_bootstrap.js": "abe81ea2ab1ae82e901787cd7f472719",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
